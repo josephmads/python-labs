563a0b7ab7bc5ed43b0116013d307739034d854e
Joseph Madsen <josephmads@protonmail.com>
2022-05-19 22:20:04 -0500

    Exercises complete for 03

--- python-201-main/03_file-input-output/03_04_pathlib_reader.py
+++ python-201-main/03_file-input-output/03_04_pathlib_reader.py
@@ -1,3 +1,41 @@
 # Refactor your file counter script to use `pathlib` also for
 # reading and writing to your CSV file. Make sure to handle the
 # path in a way so that you can run the script from anywhere.
+
+from pathlib import Path
+import csv
+
+desktop = Path('/home/joe/Desktop')
+
+text_folder = Path('/home/joe/Desktop/textfolder')
+jpg_folder = Path('/home/joe/Desktop/jpgfolder')
+
+file_dict = {'' : 0, 'txt' : 0, 'jpg' : 0}
+
+
+for filepath in desktop.iterdir():
+
+    if filepath.suffix == '':
+        file_dict[''] += 1
+
+    if filepath.suffix == '.txt':
+        file_dict['txt'] += 1
+        
+        if file_dict['txt'] > 3:
+            text_folder.mkdir(exist_ok=True)
+            new_text_path = text_folder.joinpath(filepath.name)
+            filepath.replace(new_text_path)
+
+
+    elif filepath.suffix == '.jpg':
+        file_dict['jpg'] += 1
+
+        if file_dict['jpg'] > 3:
+            jpg_folder.mkdir(exist_ok=True)
+            new_jpg_path = jpg_folder.joinpath(filepath.name)
+            filepath.replace(new_jpg_path)
+
+with open(desktop.joinpath("filescount.csv"), "a") as csvfile:
+    countswriter = csv.writer(csvfile)
+    data = [file_dict[''], file_dict['txt'], file_dict['jpg']]
+    countswriter.writerow(data)
--- python-201-main/03_file-input-output/03_05_more-filetypes.py
+++ python-201-main/03_file-input-output/03_05_more-filetypes.py
@@ -2,3 +2,60 @@
 # in your CSV file. Remember that the format of your output needs to be
 # consistent across multiple runs of your script. This means you'll need to
 # make a compromise and choose which file types you want to record beforehand.
+
+from pathlib import Path
+import csv
+import pdb
+
+desktop = Path('/home/joe/Desktop')
+
+text_folder = Path('/home/joe/Desktop/textfolder')
+jpg_folder = Path('/home/joe/Desktop/jpgfolder')
+
+file_dict = {'' : 0, 'txt' : 0, 'jpg' : 0, 'png' : 0, 'md' : 0, 'csv' : 0}
+csv_header = ['FILES', 'TXT', 'JPG', 'PNG', 'MD', 'CSV']
+
+for filepath in desktop.iterdir():
+
+    if filepath.suffix == '':
+        file_dict[''] += 1
+
+    if filepath.suffix == '.txt':
+        file_dict['txt'] += 1
+        
+        if file_dict['txt'] > 3:
+            text_folder.mkdir(exist_ok=True)
+            new_text_path = text_folder.joinpath(filepath.name)
+            filepath.replace(new_text_path)
+
+    if filepath.suffix == '.jpg':
+        file_dict['jpg'] += 1
+
+        if file_dict['jpg'] > 3:
+            jpg_folder.mkdir(exist_ok=True)
+            new_jpg_path = jpg_folder.joinpath(filepath.name)
+            filepath.replace(new_jpg_path)
+
+    if filepath.suffix == '.png':
+        file_dict['png'] += 1
+
+    if filepath.suffix == '.md':
+        file_dict['md'] += 1
+
+    elif filepath.suffix == '.csv':
+        file_dict['csv'] += 1
+
+
+with open(desktop.joinpath('filescount.csv'), 'a') as csvfile:
+    row_writer = csv.writer(csvfile)
+
+    tester = open(desktop.joinpath('filescount.csv'), 'r')
+    test_line = tester.readline()
+    if csv_header[0] not in test_line: 
+        row_writer.writerow(csv_header)
+    tester.close()
+
+    data = [file_dict[''], file_dict['txt'], file_dict['jpg'], file_dict['png'], file_dict['md'], file_dict['csv']]
+    row_writer.writerow(data)
+
+    
