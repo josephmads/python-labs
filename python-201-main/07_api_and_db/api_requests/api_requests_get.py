0d25554659b031cb10b5c59b9dcabc931748ad3d
Joseph Madsen <josephmads@protonmail.com>
2022-06-21 06:26:11 -0500

    Created 'api_requests_post.py

--- python-201-main/07_api_and_db/api_requests/api_requests_get.py
+++ python-201-main/07_api_and_db/api_requests/api_requests_get.py
@@ -4,9 +4,11 @@ from pprint import pprint
 base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
 params_dict = {"first_name": "test"}
 
-response = requests.get(base_url, params=params_dict)
+response = requests.get(base_url)
 
-pprint(response.json())
+data = (response.json())
+
+pprint(data['data'][0]['first_name'])
 
 # print(f"Response Content: {response.content}")
 # print(f"Response Status Code: {response.status_code}")
---
+++ python-201-main/07_api_and_db/api_requests/api_requests_post.py
@@ -0,0 +1,17 @@
+import requests
+
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+
+body = {
+    "first_name": "Leroy",
+    "last_name": "Jenkins",
+    "email": "oldschool@memes.biz"
+    }
+
+response = requests.post(base_url, json=body)
+
+print(f"Response Code: {response.status_code}")
+
+response = requests.get(base_url)
+
+print(f"Response Content: {response.content}")
