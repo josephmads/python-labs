422f156d7e430a41b7a6e79a0ca46121f20af6f0
Joseph Madsen <josephmads@protonmail.com>
2022-07-17 20:56:03 -0500

    Finished Exercise_03

--- python-apis-databases/databases/Exercise_03.py
+++ python-apis-databases/databases/Exercise_03.py
@@ -3,3 +3,18 @@ Update all films in the film table to a rental_duration value of 10,
 if the length of the movie is more than 150.
 
 '''
+
+import sqlalchemy
+import os
+
+password = os.environ["PASSWORD"]
+
+engine = sqlalchemy.create_engine(f"mysql+pymysql://joe:{password}@localhost/sakila")
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
+
+film = sqlalchemy.Table("film", metadata, autoload=True, autoload_with=engine)
+
+query = sqlalchemy.update(film).values(rental_duration=10).where(film.columns.length > 150)
+
+result_proxy = connection.execute(query)
