57b72944425ee36471e564dab0b1491cd77834f2
Joseph Madsen <josephmads@protonmail.com>
2022-08-09 06:22:12 -0500

    Finished project Name Game

--- python-201-main/projects/api-name-game.py
+++ python-201-main/projects/api-name-game.py
@@ -1,1 +1,13 @@
 # Add an API call to your CLI game that assigns a name to your player.
+
+import requests
+
+min_name_len = 4
+
+max_name_len = 9
+
+base_url = f"https://uzby.com/api.php?min={min_name_len}&max={max_name_len}"
+
+response = requests.get(base_url)
+
+print(response.text)
