7833dc9bf52cbd7f93d17a7f016434503f423713
Joseph Madsen <josephmads@protonmail.com>
2022-08-17 06:30:08 -0500

    Finished barking-shibes.py exercise

--- python-201-main/projects/barking-shibes.py
+++ python-201-main/projects/barking-shibes.py
@@ -7,3 +7,28 @@
 # Write the code logic to make the API calls and assign the output to
 # `doged_quote` and `doge_image_url` respectively.
 # Then write the `html` string to a `.html` file and look at it in your browser.
+
+
+from pathlib import Path
+import requests
+
+# Image API
+
+img_url = "http://shibe.online/api/shibes?count=1"
+img = requests.get(img_url).json()[0]
+
+# Quotes API
+
+quote_url = "http://quotes.stormconsultancy.co.uk/random.json"
+quote = requests.get(quote_url).json()["quote"]
+author = requests.get(quote_url).json()["author"]
+
+# HTML Structure
+
+html = f"<p>{quote} - {author}</p><img src='{img}'>"
+
+# Write to file
+
+f = Path().home().joinpath("Desktop").joinpath("index.html")
+
+f.write_text(html)
