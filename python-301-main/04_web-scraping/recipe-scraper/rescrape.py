ead40ed8dc18b1eacf8dbee727e9845ecba097ef
Joseph Madsen <josephmads@protonmail.com>
2022-09-26 06:17:56 -0500

    Finished python-301-04 exercises

--- python-301-main/04_web-scraping/recipe-scraper/rescrape.py
+++ python-301-main/04_web-scraping/recipe-scraper/rescrape.py
@@ -8,4 +8,89 @@
 # into the code base, to get some additional practice in working with your
 # custom Python classes.
 
-URL = "https://codingnomads.github.io/recipes"
+
+import requests
+from bs4 import BeautifulSoup
+
+class Ingredient:
+    """Models an Ingredient."""
+
+    def __init__(self, name, amount):
+        self.name = name
+        self.amount = amount
+
+    def expire(self):
+        """Expires the ingredient item."""
+        print(f"whoops, these {self.name} went bad...")
+        self.name = "expired " + self.name
+
+    def __str__(self):
+        return f"You have {self.amount} {self.name}."
+
+def user_ingredients():
+
+    user_input1 = input("Enter the name of your ingredient: ")
+    user_input2 = input(f"Enter the amount of {user_input1} you have: ")
+    user_ing = Ingredient(user_input1, user_input2)
+    return ingredients_list.append(user_ing.name)
+
+# Gather ingredients
+
+ingredients_list = []
+recipes_list = []
+
+user_ingredients()
+
+gathering = True
+
+while gathering == True:
+    more = input("Do you have more ingredients? [Y]es/[N]o: ")
+    if more.lower() == "y":
+        user_ingredients()
+        continue
+    elif more.lower() == "n":
+        print("Ok. Let's find some recipes to make with your ingredients.")
+        gathering = False
+        continue
+    else:
+        print("I'll take that as a no.\n" 
+        "Let's find some recipes to make with your ingredients.")
+        gathering = False
+        continue
+
+ 
+
+base_url = "https://codingnomads.github.io/recipes/"
+
+link_page = requests.get(base_url)
+
+link_soup = BeautifulSoup(link_page.text, features="html.parser")
+links = link_soup.find_all("a")
+
+link_list = [(link["href"]) for link in links]
+
+for link in link_list:
+    
+    URL = f"https://codingnomads.github.io/recipes/{link}"
+
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.text, features="html.parser")
+
+    title = soup.find("h1", class_="title")
+    author = soup.find("p", class_="author")
+    recipe = soup.find("div", class_="md")
+
+    for ingredient in ingredients_list:
+        search = ingredient in recipe.text
+        if search == True:
+            recipes_list.append(title.text)
+        else:
+            pass
+    breakpoint()
+
+    # if ingredients found in recipe return title. make a function
+
+print("Here are the recipes:")
+
+for recipe in recipes_list:
+    print(recipe)
